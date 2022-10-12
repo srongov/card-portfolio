e0b45b11801fefb512055d3998eb8bab35382303
Hak Gov <srongov@yahoo.com>
2022-10-12 04:32:16 -0400

    project change

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,22 +1,32 @@
 import React from "react";
 
-import project from "../image/project.PNG"
+import project from "../image/cookbook.png"
 import project2 from "../image/project2.PNG"
 
 export default function Project() {
     return (
         <div className="project">
             <h1>Project</h1>
-            <div className="top-project">
-                <div id="project-div">
-                    <p>Cook Book</p>
-                    <img src={project} alt="project" />
+            <div className='project-container'>
+                <div className="card">
+                    <a href="https://github.com/AmyShafer/Mad-Archers-Cookbook"><img src={project} alt="html" /></a>
+                    <div id='top-card'>
+                        <p>Cook Book</p>
+                    </div>
+                    <div id="name">
+                        <p>sadfasdfasdfadfZed is the leader of the Order of Shadow </p>
+                    </div>
                 </div>
-                <div id="project-div">
-                    <p>Drum Game</p>
-                <img src={project2} alt="project" />
+                <div className="card">
+                    <img src={project} alt="html" />
+                    <div id='top-card'>
+                        <p>Cook Book</p>
+                    </div>
+                    <div id="name">
+                        <p>Zed is tasdfasdfasdhe leader of the Order of Shadow </p>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
