473888f9d92a1b6e09ef23517d72ea2a27e445c5
Hak Gov <srongov@yahoo.com>
2022-10-14 22:16:59 -0400

    delte contact nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     return (
         <div className="nav">
             <div className="logo">
-            {/* <img src={image1} alt="Hak" /> */}
+                {/* <img src={image1} alt="Hak" /> */}
             </div>
             <div className="list">
                 <li><a href="#about">Home</a></li>
@@ -15,7 +15,6 @@ export default function Navbar() {
                 <li><a href="#skill">Skill</a></li>
                 <li><a href="#project">Project</a></li>
                 <li><a href="resume">Resume</a></li>
-                <li><a href="contact">Contact</a></li>
             </div>
         </div>
     )
